b76ca4837740e6093a5a924ddbe37863189344c6
Jacob Joseph Jorishie <jake.jorishie@Jacobs-MBP.attlocal.net>
2022-08-06 19:03:31 -0500

    logic done

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -4,8 +4,51 @@ import './TicTacToe.css';
 const TicTacToe = () => {
 const [turn, setTurn] = useState('X');
 const [cells, setCells] = useState(Array(9).fill(''));
+const [winner, setwinner] = useState(null);
+
+
+
+const checkWinner = (squares) => {
+let combos = {
+    across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ],
+    down: [
+        [0, 3, 6],
+        [1, 4, 7], 
+        [2, 5, 8]
+    ],
+    diagnol: [
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+}
+for (let combo in combos) {
+    combos[combo].forEach((pattern) => {
+        if (
+            squares[pattern[0]] === '' ||
+            squares[pattern[1]] === '' ||
+            squares[pattern[2]] === ''
+        ) {
+        } else if (
+            squares[pattern[0]] === squares[pattern[1]] &&
+            squares[pattern[1]] === squares[pattern[2]]
+        ) {
+            setwinner(squares[pattern[0]])
+        }
+    })
+
+}
+
+}
 
     const handleClick = (num) => {
+        if (cells[num] !== '') {
+            alert('Space Already Taken')
+            return
+        }
     let squares = [...cells];
         if (turn === 'X') {
             squares[num] = 'X';
@@ -14,13 +57,17 @@ const [cells, setCells] = useState(Array(9).fill(''));
             squares[num] = 'O';
             setTurn ('X');
         }
+        checkWinner(squares)
         setCells(squares)
         console.log(squares)
     };
-    
+    const handleRestart = () => {
+        setwinner(null);
+        setCells(Array(9).fill(''));
+    }
     
     const Cell = ({num}) => {
-        return <td onClick={() => handleClick(num)}>-</td>
+        return <td onClick={() => handleClick(num)}>{cells[num]}</td>
     }
 
     return (
@@ -29,22 +76,28 @@ const [cells, setCells] = useState(Array(9).fill(''));
                         Turn: {turn}
                         <tbody>
                             <tr>
-                                <Cell num={1} />
-                                <Cell num={2}/>
-                                <Cell num={3} />
+                                <Cell num={0} />
+                                <Cell num={1}/>
+                                <Cell num={2} />
                             </tr>
                             <tr>
+                                <Cell num={3} />
                                 <Cell num={4} />
                                 <Cell num={5} />
-                                <Cell num={6} />
                             </tr>
                             <tr>
+                                <Cell num={6} />
                                 <Cell num={7} />
                                 <Cell num={8} />
-                                <Cell num={9} />
                             </tr>
                         </tbody>
                     </table>
+                    {winner && (
+                    <span>
+                    <h1 id= "winner">{winner} is the Winner!!!</h1>
+                    <button id= "play-again" onClick={() => handleRestart ()}>Play Again</button>
+                    </span>
+                    )}
                 </div>
       );
 }
